debb8abad422d62bbcce532d5791f98d3364b770
0KhaLNayaK0 <115925245+0KhaLNayaK0@users.noreply.github.com>
2022-10-16 16:14:06 +0530

    Add files via upload

---
+++ celsiustoferenheit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    // This code is to convert celsius into ferenheit
+    float a;
+    printf("Enter the temperature in degree celsius \n");
+    scanf("%f" , &a );
+    printf("The temperature in ferenheit is %f " , (a*(9/5))+32);
+    return 0;
+}
